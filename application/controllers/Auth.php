73f21ffb3ed57b5eedbb4724d9e25a0d219ba125
A00770012 <A00770012bcit@gmail.com>
2015-04-01 20:22:41 -0700

    complete the functionality of Auth controller Signed-off-by:A00770012 <A00770012bcit@gmail.com>

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -12,4 +12,23 @@ class Auth extends Application {
         $this->render();
     }
 
+    // submit method to handle form submission from the login view page
+    function submit() {
+        $key = $_POST['userid'];
+        $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+        $user = $this->users->get($key);
+        if ($password == (string) $user->password) {
+            $this->session->set_userdata('userID', $key);
+            $this->session->set_userdata('userName', $user->name);
+            $this->session->set_userdata('userRole', $user->role);
+        }
+        redirect('/');
+    }
+
+    // logout method to handle logout and clear the session value
+    function logout() {
+        $this->session->sess_destroy();
+        redirect('/');
+    }
+
 }
