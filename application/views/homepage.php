172cc6373765a19b795d7c959e0c77d023f3bc31
A00770012 <A00770012bcit@gmail.com>
2015-04-01 20:15:46 -0700

    added the user login form into home page Signed-off-by:A00770012 <A00770012bcit@gmail.com>

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -3,5 +3,9 @@
 
     <p>I told my mother-in-law that my house was her house, and she said, 'Get the hell off my property.'</p>
     <h4>Joan Rivers</h4>
-        
+    <form name="login" method="post" action="/auth/login">
+        UserID: <input type="text" name="userid"></input><br/>
+        Password: <input type="password" name="password"></input><br/>
+        <input type="submit">Submit</input>
+    </form>  
 </div>
