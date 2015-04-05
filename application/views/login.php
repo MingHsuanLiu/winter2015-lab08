8e34a17f9a5c3403451ecf9198c14e6e07f9042a
A00770012 <A00770012bcit@gmail.com>
2015-04-05 14:32:49 -0700

    fix the type po Signed-off-by:A00770012 <A00770012bcit@gmail.com>

--- application/views/login.php
+++ application/views/login.php
@@ -1,7 +1,5 @@
-<div class="row">
-    <form name="login" method="post" action="/auth/submit">
-        UserID: <input type="text" name="userid"></input><br/>
-        Password: <input type="password" name="password"></input><br/>
-        <input type="submit">Submit</input>
-    </form>
-</div>
+<form name="login" method="post" action="/auth/submit">
+    UserID: <input type="text" name="userid"></input><br/>
+    Password: <input type="password" name="password"></input><br/>
+    <input type="submit">Submit</input>
+</form>
